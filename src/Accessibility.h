575ee162b084b7f2f9746e2d39e36917b0412115
Aaron Smith <aaronsmith@Aaron-Smiths-MacBook-Pro.local>
2009-09-07 18:35:26 -0700

    Deletes

--- src/Accessibility.h
+++
@@ -1,6 +0,0 @@
-//copyright aaronsmith 2009
-
-#import "GDAccessibilityManager.h"
-#import "GDAccessibilityObserver.h"
-#import "GDAccessibilityNotification.h"
-#import "GDAccessibilityOperationResult.h"
