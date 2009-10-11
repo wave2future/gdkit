8c40b9b92abbbb505f6d7dddbb856de15a1828f7
Aaron Smith <aaronsmith@Aaron-Smiths-MacBook-Pro.local>
2009-10-10 19:36:07 -0700

    not needed

--- GDAccessibilityObserverHelpers.h
+++
@@ -1,9 +0,0 @@
-//copyright 2009 aaronsmith
-
-#import <Cocoa/Cocoa.h>
-
-@interface GDAccessibilityObserverHelpers : NSObject {
-
-}
-
-@end
--- GDAccessibilityObserverHelpers.m
+++
@@ -1,7 +0,0 @@
-//copyright 2009 aaronsmith
-
-#import "GDAccessibilityObserverHelpers.h"
-
-@implementation GDAccessibilityObserverHelpers
-
-@end
