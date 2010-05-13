c6facbbf3131ecaa702e292eb04a5ecfc1f53ae8
Aaron Smith <aaronsmith@Aaron-Smiths-MacBook-Pro.local>
2010-05-13 00:49:17 -0700

    added the initial mac app to demonstrate gdkit interface builder plugins. this will be deleted later.

---
+++ examples/mac/Scale9/Scale9AppDelegate.m
@@ -0,0 +1,15 @@
+
+#import "Scale9AppDelegate.h"
+
+@implementation Scale9AppDelegate
+@synthesize window;
+@synthesize button;
+
+- (void) applicationDidFinishLaunching:(NSNotification *) aNotification {
+}
+
+- (void) test:(id) sender {
+	NSLog(@"test");
+}
+
+@end
---
+++ examples/mac/Scale9/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Scale9
+//
+//  Created by Aaron Smith on 5/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
