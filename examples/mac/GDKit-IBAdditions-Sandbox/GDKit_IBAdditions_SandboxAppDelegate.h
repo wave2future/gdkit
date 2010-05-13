e31ca7c4fb3d16193e3aef7e2e41562ee8d4585c
Aaron Smith <aaronsmith@Aaron-Smiths-MacBook-Pro.local>
2010-05-13 00:48:48 -0700

    added a sample mac app to demonstrate the interface builder plugins

---
+++ examples/mac/GDKit-IBAdditions-Sandbox/GDKit_IBAdditions_SandboxAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  GDKit_IBAdditions_SandboxAppDelegate.h
+//  GDKit-IBAdditions-Sandbox
+//
+//  Created by Aaron Smith on 5/13/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GDKit_IBAdditions_SandboxAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ examples/mac/GDKit-IBAdditions-Sandbox/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GDKit-IBAdditions-Sandbox
+//
+//  Created by Aaron Smith on 5/13/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
