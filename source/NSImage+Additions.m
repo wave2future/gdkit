44c271f225d99f1a3718d408fd46cb421c793e37
Aaron Smith <aaronsmith@Aaron-Smiths-MacBook-Pro.local>
2010-05-16 23:33:28 -0700

    added addition to NSImage to get a CGImageRef

---
+++ source/NSImage+Additions.m
@@ -0,0 +1,14 @@
+
+#import "NSImage+Additions.h"
+
+@implementation NSImage (Additions)
+
+- (CGImageRef) CGImage {
+	CGImageSourceRef source = CGImageSourceCreateWithData((CFDataRef)[self TIFFRepresentation],NULL);
+	CGImageRef ref = CGImageSourceCreateImageAtIndex(source,0,NULL);
+	CFRelease(source);
+	//CGImageRelease(ref);
+	return ref;
+}
+
+@end
