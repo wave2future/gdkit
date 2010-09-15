56d5ee7d7c17202c233cc6d14ba4e7ecafe40780
Aaron Smith <aaronsmith@17.local>
2010-09-15 00:04:17 -0700

    moved old crash reporter.. this will eventually go away

---
+++ examples/mac/CrashReporter__OLD/source/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CrashReporter
+//
+//  Created by Aaron Smith on 12/1/09.
+//  Copyright 2009 Mccann Worldgroup. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
